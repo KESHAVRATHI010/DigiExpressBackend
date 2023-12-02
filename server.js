2225ac8dcea55e1f392381231bda4e59609464d5
CodebreakerEntity303 <codebreakerentity303@gmail.com>
2023-12-02 16:51:53 +0530

    updated

--- server.js
+++ server.js
@@ -4,9 +4,12 @@ const express = require("express")
 const app=express()
 const mongoose= require("mongoose")
 
-mongoose.connect(process.env.database_url);
+mongoose.connect(process.env.database_url,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
 const db=mongoose.connection
-db.on('error',(error)=> console.log(error))
+db.on('error',(error)=> console.error('Error connecting to the database:',error))
 db.once('open', ()=> console.log("connected to database"))
 
 app.use(express.json())
@@ -14,5 +17,5 @@ app.use(express.json())
 const productrouter = require('./routes/products');
 app.use('/product',productrouter)
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT|| 3000
 app.listen(PORT,() => console.log("server started!"))
