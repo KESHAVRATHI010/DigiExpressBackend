2a2a95713f33820c9c2127158f934f2980f52eed
CodebreakerEntity303 <codebreakerentity303@gmail.com>
2023-12-03 00:20:06 +0530

    updated

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ require('dotenv').config()
 const express = require("express")
 const app=express()
 const cors =require("cors")
-
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*'); // You can replace '*' with your client's origin
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
