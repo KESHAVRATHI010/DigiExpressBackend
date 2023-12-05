b42d4191811fa31e131e954e3e61980bd323ea7d
CodebreakerEntity303 <codebreakerentity303@gmail.com>
2023-12-05 20:35:49 +0530

    updated

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require("express")
 const app=express()
 const cors =require("cors")
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); // You can replace '*' with your client's origin
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
