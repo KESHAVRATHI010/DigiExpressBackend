bd9cbc6568090f9321b040116fe4fd5650944e44
CodebreakerEntity303 <codebreakerentity303@gmail.com>
2023-12-16 02:16:14 +0530

    updated

--- routes/products.js
+++ routes/products.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Product = require("../models/product.js");
 
-router.get("/product", async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     const products = await Product.find();
     res.status(200).json(products);
